4e73dc7534ca8fb4ea0c575711e079fb983d3293
gsytnik <39895377+gsytnik@users.noreply.github.com.>
2025-03-09 19:15:31 -0700

    solution js exercises 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (inputString) {
+    inputString = inputString.replace(/[\.\,\!\? ]/g, '');
+    // console.log(inputString);
 
+    for (i = 0; i <= inputString.length / 2; i++) {
+        if (inputString.charAt(i).toLowerCase() 
+                != inputString.charAt(inputString.length - i - 1).toLowerCase()) {
+            return false;
+        }    
+    }
+
+    return true;
 };
 
 // Do not edit below this line
