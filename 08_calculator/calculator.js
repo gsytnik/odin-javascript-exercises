9fa63cd683b9b6029173290ee20b1c522c7af6ad
gsytnik <39895377+gsytnik@users.noreply.github.com.>
2025-03-09 17:41:48 -0700

    solution js intro 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	return numbers.reduce((total, num) => {
+    return total + num;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  return numbers.reduce((total, num) => total * num, 1)
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(end) {
+  
+  let total = 1;
+	for (i = 2; i <= end; i++) {
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
