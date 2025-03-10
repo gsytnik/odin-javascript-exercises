db6717fe1e8445eae7cb72814060c82154fc7e14
gsytnik <39895377+gsytnik@users.noreply.github.com.>
2025-03-09 19:23:55 -0700

    solution js intro 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(entry) {
+    entry = parseInt(entry);
 
+    let fib_store = [1, 1, 2, 3];
+
+    for (i = 3; i < entry - 1; i++) {
+        fib_store.push(fib_store[i] + fib_store[i - 1])
+    }
+
+    return entry < 0 ? "OOPS" : (entry === 0 ? 0 : fib_store[entry - 1]);
 };
 
 // Do not edit below this line
