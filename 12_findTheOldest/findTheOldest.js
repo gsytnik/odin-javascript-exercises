792f21b1d3ed8f6445366cc4c164cc0dcb72d865
gsytnik <39895377+gsytnik@users.noreply.github.com.>
2025-03-09 19:39:49 -0700

    solution js intro 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce((person, currentPerson) => {
+        let older = (
+            (('yearOfDeath' in person ? person.yearOfDeath : 2025) - person.yearOfBirth) 
+            - (('yearOfDeath' in currentPerson ? currentPerson.yearOfDeath : 2025) - currentPerson.yearOfBirth)
+        );
 
+        console.log(person, currentPerson, older);
+        return older >= 0 ? person : currentPerson;
+    }, people[0]);
 };
 
 // Do not edit below this line
